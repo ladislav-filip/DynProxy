df85765227bfb945f974f6749ef9a137e49b75a4
LF <ladislav.filip@fullsys.cz>
2018-01-08 21:49:55 +0100

    Inicializac II.

---
+++ MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DynProxy
+{
+    public class MyClass
+    {
+        public virtual bool Flag { get; set; }
+
+        public virtual void Execute()
+        {
+            Console.WriteLine("Execute method called");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+
+namespace DynProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = new ProxyGenerator()
+                .CreateClassProxy<MyClass>(
+                    new Interceptor());
+            Work(proxy);
+
+            Console.WriteLine("Finnish. Press any key.");
+            Console.ReadKey();
+        }
+
+        static private void Work(MyClass obj)
+        {
+            obj.Flag = true;
+            obj.Execute();
+        }
+    }
+}
